02b435e53f10251935884c232d5dfedf893c59f4
nomi511 <thebespokeone@gmail.com>
2022-02-21 19:03:02 +0500

    security packages installation

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ require("express-async-errors")
 
 const express = require("express")
 const db = require("./db/connect")
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const limiter = require("express-rate-limit")
+
 
 const app = express();
 
+
+// security
+app.set('trust proxy', 1)
+app.use(limiter({
+    windowMs: 5 * 60 * 1000, // 10 minutes
+    max: 100 // limit each ip to 100 requests per 5 min
+}))
+
 app.use(express.json())
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 
 // --- bodyparser
 const bodyParser = require("body-parser")
